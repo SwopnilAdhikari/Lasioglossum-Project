662aaaa04c8594fe43238aa59fc253b0a41fed8c
Swopnil Adhikari <workadhikari@gmail.com>
2023-07-27 11:23:48 -0700

    Added P-Value Labels to Graph

--- AMNH Size Data.R
+++ AMNH Size Data.R
@@ -7,7 +7,8 @@ amnhdata <- read_excel(here("data", "amnhsize.xlsx"))
 ############################################################
 massvsmmat <- ggplot(data = amnhdata, aes(x = meanmat, y = mass)) 
 
-massvsmmat + geom_point() + geom_smooth(method = "lm") + labs(
+massvsmmat + geom_point() + geom_smooth(method = "lm") + 
+  annotate("text", x = 3, y = .08, label = "P-Value = 0.0024") + labs(
   x = "Mean Annual Temperature(C)",
   y = "Mass(g)",
   title = "Mass(g) vs Mean Annual Temperature"
@@ -16,7 +17,8 @@ lmMassvsmmat <- lm(mass ~ meanmat, data = amnhdata)
 summary(lmMassvsmmat)
 ###########################################################
 massvsyear <- ggplot(data = amnhdata, aes(x = year, y = mass))
-massvsyear + geom_point() + geom_smooth(method = "lm") + labs(
+massvsyear + geom_point() + geom_smooth(method = "lm") +
+annotate("text", x = 1918, y = 0.08, label = "P-Value = 3.358e-05") + labs(
   x = "Year",
   y = "Mass(g)",
   title = "Mass vs Year Collected"
@@ -25,7 +27,8 @@ lmMassvsyear <- lm(mass ~ year, data = amnhdata)
 summary(lmMassvsyear)
 ###########################################################
 massvsmaxt <- ggplot(data = amnhdata, aes(x = maxt, y = mass))
-massvsmaxt + geom_point() + geom_smooth(method = "lm") + labs(
+massvsmaxt + geom_point() + geom_smooth(method = "lm") + 
+  annotate("text", x = 15, y = 0.09, label = "P-Value = 3.05e-04") + labs(
   x = "Temperature(C)",
   y = "Mass(g)",
   title = "Mass vs Max Temperature in Month of Collection"
@@ -34,7 +37,8 @@ lmMassvsmaxt <- lm(mass ~ maxt, data = amnhdata)
 summary(lmMassvsmaxt)
 ###########################################################
 massvsmonth <- ggplot(data = amnhdata, aes(x = month, y = mass))
-massvsmonth + geom_point() + geom_smooth(method = "lm") + labs(
+massvsmonth + geom_point() + geom_smooth(method = "lm") + 
+  annotate("text", x = 3.2, y = 0.09, label = "P-Value = 0.21") + labs(
   x = "Month",
   y = "Mass(g)",
   title = "Correlation of Mass to Month of Collection"
@@ -43,7 +47,8 @@ lmMassvsmonth <- lm(mass ~ month, data = amnhdata)
 summary(lmMassvsmonth)
 ###########################################################
 itxthoraxvsmmat <- ggplot(data = amnhdata, aes(x = meanmat, y = itxthorax))
-itxthoraxvsmmat + geom_point() + geom_smooth(method = "lm") + labs(
+itxthoraxvsmmat + geom_point() + geom_smooth(method = "lm") + 
+  annotate("text", x = 3, y = 0.036, label = "P-Value = 0.1585") + labs(
   x = "Temperature(C)",
   y = "IT x Thorax(cm)",
   title = "IT x Thorax vs Mean Annual Temperature"
@@ -52,7 +57,8 @@ lmItxthoraxvsmmat <- lm(itxthorax ~ meanmat, data = amnhdata)
 summary(lmItxthoraxvsmmat)
 ##########################################################
 massvsmatybc <- ggplot(data = amnhdata, aes(x = matybc, y = mass))
-massvsmatybc + geom_point() + geom_smooth(method = "lm") + labs(
+massvsmatybc + geom_point() + geom_smooth(method = "lm") + 
+  annotate("text", x = 3.5, y = 0.09, label = "P-Value = 2.8e-02") + labs(
   x = "Temperature(C)",
   y = "Mass(g)",
   title = "Mass vs Mean Annual Temperature(Year of Birth)"
@@ -61,16 +67,14 @@ lmMassvsmatybc <- lm(mass ~ matybc, data = amnhdata)
 summary(lmMassvsmatybc)
 ##########################################################
 itxthoraxvsmatybc <- ggplot(data = amnhdata, aes(x = itxthorax, y = matybc))
-itxthoraxvsmatybc + geom_point() + geom_smooth(method = "lm") + labs(
+itxthoraxvsmatybc + geom_point() + geom_smooth(method = "lm") + 
+  annotate("text", x = 0.018, y = 25, label = "P-Value = 0.112")+ labs(
   x = "IT x Thorax(cm)",
   y = "Temperature(C)",
   title = "IT x Thorax vs Mean Annual Temperature(Year of Birth)"
 )
 lmitxthoraxvsmatybc <- lm(itxthorax ~ matybc, data = amnhdata)
 summary(lmitxthoraxvsmatybc) 
-
-
-
 ############################################################
 head(amnhdata, 5)
 hist(amnhdata$mass)
@@ -78,5 +82,11 @@ lines(density(amnhdata$mass), col = 'green', lwd = 1)
 ############################################################
 qqnorm(amnhdata$mass, pch = 1, frame = FALSE)  
 qqline(amnhdata$mass, col = "green", lwd = 2)
+###########################################################
+logData <- log10(amnhdata$mass)
+hist(logData)
+lines(density(logData), col = "green", lwd = 2)
 ############################################################
+
+
 
